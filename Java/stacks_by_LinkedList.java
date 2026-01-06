3ce34a2a458be9cb11d521b100fb65527a263076
Krish <kkhandelwal292@gamail.com>
2026-01-06 20:30:22 +0530

    Added Stack Implementation using Array

---
+++ Java/stacks_by_LinkedList.java
@@ -0,0 +1,26 @@
+public class stacks_by_LinkedList {
+     static class Stack{
+         static class Node{
+            Node next = null;
+            int val;
+            private Node(int val){
+                this.val = val;
+                this.next = null;
+            }
+             private Node head; // top of stack
+
+        // Constructor
+            Stack() {
+            head = null;
+         }
+            public static void push(int a ){
+                if(head== null){
+
+                }
+            }
+            public static void main(String[] args) {
+             Stack a = new Stack(3);
+            }
+        }
+    }
+}
