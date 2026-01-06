66f9736ffd876a8bea1a2a479918e369e8ffa9c4
Krish <kkhandelwal292@gamail.com>
2026-01-06 22:35:36 +0530

    Added Stack Implementation using Array

--- Java/stacks_by_LinkedList.java
+++ Java/stacks_by_LinkedList.java
@@ -1,26 +1,81 @@
 public class stacks_by_LinkedList {
-     static class Stack{
-         static class Node{
-            Node next = null;
+
+    static class Stack {
+        int size =0;
+        static class Node {
             int val;
-            private Node(int val){
+            Node next;
+
+            private Node(int val) {
                 this.val = val;
                 this.next = null;
             }
-             private Node head; // top of stack
+        }
 
-        // Constructor
-            Stack() {
+        Node head; // top of stack
+
+        Stack() {
             head = null;
-         }
-            public static void push(int a ){
-                if(head== null){
+        }
 
-                }
+        // PUSH → O(1)
+        public void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+            size++;
+        }
+
+        // POP → O(1)
+        public int pop() {
+            if (head == null) {
+                System.out.println("Stack Underflow");
+                return -1;
             }
-            public static void main(String[] args) {
-             Stack a = new Stack(3);
+            int val = head.val;
+            head = head.next;
+            size--;
+            return val;
+            
+        }
+
+        // DISPLAY
+        private void display() {
+            if (head == null) {
+                System.out.println("Stack is empty");
+                return;
             }
+            Node temp = head;
+            System.out.println("Stack elements:");
+            while (temp != null) {
+                System.out.println(temp.val);
+                temp = temp.next;
+            }
+        }
+        //ISEMPTY → O(1)
+        private boolean isEmpty() {
+            return head == null;
+        }
+
+        // size
+        private int size() {
+            return size;
+        }
+        public static void main(String[] args) {
+            Stack st = new Stack();
+
+            st.push(3);
+            st.push(5);
+            st.push(7);
+            System.out.println("Size: " + st.size());
+            st.display();
+
+            int b = st.pop();
+            System.out.println("Popped: " + b);
+            System.out.println(st.isEmpty());
+            System.out.println("Size: " + st.size());
+            st.display();
+
         }
     }
 }
