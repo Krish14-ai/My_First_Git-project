c17ff2de4f9a23569b10ca13f04af3a3b03ddfa1
Krish <kkhandelwal292@gamail.com>
2026-01-10 23:00:25 +0530

    Added Link list file

--- Java/link_list.java
+++ Java/link_list.java
@@ -1,3 +1,19 @@
 public class link_list {
+    public class Node{
+        int data;
+        Node next;
+    }
+    link_list(){
+        this.size=0;
+    }
+    private Node head;
+    private int size;
     
+    public void add(int data){
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
