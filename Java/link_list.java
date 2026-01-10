acb53a4b56c4cabbec795cfad2e07c4cfe91387c
Krish <kkhandelwal292@gamail.com>
2026-01-10 23:02:14 +0530

    added methods

--- Java/link_list.java
+++ Java/link_list.java
@@ -10,7 +10,32 @@ public class link_list {
     private int size;
     
     public void add(int data){
-        
+        Node newNode=new Node();
+        newNode.data=data;
+        newNode.next=null;
+        if(head==null){
+            head=newNode;
+            size++;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        size++;
+    }
+    public void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" -> ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+      
+    public int getSize(){
+        return size;
     }
 
     public static void main(String[] args) {
