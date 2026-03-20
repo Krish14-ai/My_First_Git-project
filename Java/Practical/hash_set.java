767ac3fd6f34b49ec16a5faee19cd80db54b454e
Krish14-ai <kkhandelwal292@gmail.com>
2026-03-20 22:27:45 +0530

    Added Hashset file

---
+++ Java/Practical/hash_set.java
@@ -0,0 +1,17 @@
+public class hash_set {
+    public class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public class Hashset {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
